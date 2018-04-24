3a00b6a7d7efc757304dcf48235eef333eb9d2d0
Hugo Cunha <hugo.cunha@globaltronic.pt>
2018-04-24 17:39:43 +0100

    Add sampling rate config on SD card Minor bugfix Add comments Cleanup examples

--- BAGA/examples/DataLoggerClockSleep/DataLoggerClockSleep.ino
+++ BAGA/examples/DataLoggerClockSleep/DataLoggerClockSleep.ino
@@ -8,7 +8,7 @@
 #include <SWClock.h>
 
 /************************************************************************
-* BAGA A1
+* BAGA A2
 * Data Logger Clock Sleep Example 
 *
 * This example reads data periodically, saves it to the MicroSD Card and
@@ -19,10 +19,6 @@
 * 	Schematic: A2
 *   PCB: A3/A4
 *************************************************************************/
-
-// Select the desired debug serial port:
-// * Serial (USB-CDC)
-// * Serial1 (J3 connector on board)
 
 /**********************
  * BAGA Configuration *
@@ -41,7 +37,7 @@ char configFilename[] = "config.txt";
 unsigned long ReadIntervalM = 1;
 
 /* Sensor reading periodicity in seconds */
-unsigned long ReadIntervalS (ReadIntervalM * 60);
+unsigned long ReadIntervalS = (ReadIntervalM * 60);
 
 /* Log file header */
 char strDataString[] = "Timestamp,Temp_C,Temp_Apar_C,Hum_Abs,Hum_Rel,Press_Abs_mBar,Rad_Sol,Vol_Bat";
@@ -56,8 +52,12 @@ char strDataString[] = "Timestamp,Temp_C,Temp_Apar_C,Hum_Abs,Hum_Rel,Press_Abs_m
  * Variables *
  *************/
 
-/* Classes objects */
+/* Macros */
+#define LF					0x0A
+#define CR					0x0D
+#define MAX_LINE_LENGTH		31
 
+/* Classes objects */
 BAGA baga; 									// BAGA instance
 SdFat sd;									// SD card instance
 SdFile logFile;								// Log file
@@ -65,11 +65,10 @@ SdFile configFile;							// Configuration file
 SWClock clock;								// SWCLock instance
 
 /* Globals */
-char configString[21];						// String to read the configuration to
+char fileString[MAX_LINE_LENGTH + 1];		// String to read each line from file
 char clockString[21];						// String to save the converted date and time
 unsigned long timestamp;					// Timestamp
 unsigned long ID = 0; 						// Measurements counter
-
 
 /* Strings */
 #if defined(BAGA_DEBUG)
@@ -78,14 +77,41 @@ char strOk[7] = "[ OK ]";
 char strFail[7] = "[FAIL]";
 #endif
 
+/*************
+ * FUNCTIONS *
+ *************/
+int readLine(SdFile *file, char * outputBuffer)
+{
+	int data;
+	int count = 0;
+
+	/* Read next char */
+	data = (*file).read();
+
+	/* Check if is the end of the file */
+	if (data == EOF)
+	{
+		return EOF;
+	}
+	
+	/* Read until an EOF, end of line or carriage return is reached */
+	while((data != EOF) && (data != LF) && (data != CR))
+	{
+		outputBuffer[count++] = data;
+		data = (*file).read();
+	}
+
+	outputBuffer[count] = '\0';
+
+	return count;
+}
+
 /*********
  * SETUP *
  *********/
 
 void setup() {
 	// put your setup code here, to run once:
-	byte result;
-
 	TXLED1;  // Disable TX LED to Save Power
     RXLED1;	 // Disable RX LED to Save Power
 
@@ -94,11 +120,7 @@ void setup() {
 	//while(!DebugSerial);
 #endif
 
-	//pinMode(20,OUTPUT);
-	//digitalWrite(20,LOW);
-
-	result = baga.begin();
-	if (result)
+	if (baga.begin())
 	{
 		#if defined(BAGA_DEBUG)
 		DebugSerial.println(strFail);
@@ -133,11 +155,53 @@ void setup() {
 	{
 		int data;
 		int count = 0;
-		
-		/* Parse the configuration */
-		while(((data = configFile.read()) >= 0) && (count < 21))
-		{
-			configString[count++] = data;
+	
+		/* Read lines from file */
+		while((count = readLine(&configFile, fileString)) != EOF)
+		{	
+			/* Check the number of bytes of each line */
+			if (count > 0)
+			{
+				/* Check if it's a Timestamp configuration line */
+				if ((fileString[0] == 'T') ||  (fileString[0] == 't'))
+				{
+					/* Initialize SWClock */
+					clock.begin(fileString+1);
+					
+					/* Save current millis value */
+					timestamp = millis();
+
+					#if defined(BAGA_DEBUG)
+					DebugSerial.print("Time: ");
+					DebugSerial.print(fileString+1);
+					DebugSerial.print(" (");
+					DebugSerial.print(count, DEC);
+					DebugSerial.println(")");
+					#endif
+				}
+				/* Check if it's a sampling Period configuration line */
+				else if ((fileString[0] == 'P') ||  (fileString[0] == 'p'))
+				{
+					/* convert the string */
+					ReadIntervalM = (unsigned long)atol(fileString+1);
+					
+					/* Read interval must be at least 1 minute */
+					if(ReadIntervalM < 1)
+					{
+						ReadIntervalM = 1;
+					}
+
+					/* Calculate the new interval value in seconds */
+					ReadIntervalS = (ReadIntervalM * 60);
+
+					#if defined(BAGA_DEBUG)
+					DebugSerial.print("Period: ");
+					DebugSerial.print(fileString+1);
+					DebugSerial.print("Converted value: ");
+					DebugSerial.println(ReadIntervalM, DEC);
+					#endif
+				}
+			}
 		}
 		
 		/* Close the file */
@@ -146,10 +210,6 @@ void setup() {
 		#if defined(BAGA_DEBUG)
 		Serial.println(configString);
 		#endif
-
-		/* Initialize SWClock */
-		clock.begin(configString);
-		timestamp = millis();
 	}
 
 	/* Open log file */
@@ -199,8 +259,6 @@ void loop() {
 	/* Disable LEDs to save power */
 	TXLED1;
     RXLED1;
-	
-	//digitalWrite(20,HIGH);
 
 	/* Turn system LED on */
 	baga.setLedOn(); // Turn LED on to indicate that a measurement is in progress
@@ -210,8 +268,6 @@ void loop() {
 	
 	/* Turn system LED off */
 	baga.setLedOff();
-
-	//digitalWrite(20,LOW);
 	
 	/* Open log file */
 	if (!logFile.open(logFileName, O_WRITE | O_CREAT | O_APPEND | O_AT_END))
@@ -227,11 +283,7 @@ void loop() {
 		String dataString = "";
 	
 		if(clock.isInitialized() == true)
-		{
-			/* Get the currente emulated date and time */
-			clock.addTime(0,0,0,0,0,(int)((millis()-timestamp)/1000)+ReadIntervalS);
-			timestamp = millis();
-			
+		{			
 			/* Build the timestamp from the emulated clock */
 			clock.getDateTimeString(clockString);
 
@@ -269,6 +321,11 @@ void loop() {
 		
 		/* Increment measurements counter */
 		ID++;
+
+		#if !defined(BAGA_DEBUG)
+		Serial1.print("Data: ");
+		Serial1.println(dataString);
+		#endif
 		
 		/* Print all the values to the log file */
 		logFile.println(dataString);
@@ -280,7 +337,14 @@ void loop() {
 		DebugSerial.println(dataString); // Print to the serial port	
 		#endif
 	}
-  	
+
 	/* Enter sleep mode to save power */
     baga.sleep(ReadIntervalS*1000, ADC_OFF);
+
+	if(clock.isInitialized() == true)
+	{
+		/* Update the emulated date and time */
+		clock.addTime(0,0,0,0,0,(int)((millis()-timestamp)/1000)+ReadIntervalS);
+		timestamp = millis();
+	}
 }
